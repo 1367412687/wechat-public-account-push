86f3bcc86933ca2329ef674d08b31dbb59cd7e54
1367412687 <112149353+1367412687@users.noreply.github.com>
2022-08-28 13:22:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ export const config = {
     // 公众号appSecret
     appSecret: "c173558437f1faad79a6c3b228cae36a",
     // 模板消息id
-    templateId: "MBzxRnaXJ8fQXGX7Iu5NdRosToids8kYtD3N96wYb_M",
+    templateId: "VjwGB4d9eI1PFC3xvcipRpM9IEPCeyIk2PVuOFlyPZo",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     user: ["o8X0y53RfHNbaJTXhEZRKo0SK-Y0"],
      
@@ -29,7 +29,6 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
